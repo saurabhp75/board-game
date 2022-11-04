ae581be8b3672497fc3e1a467976b546022b7832
Saurabh Prakash <saurabhp75@gmail.com>
2022-11-04 05:51:06 +0530

    Organise the codebase

--- src/board/Board.jsx
+++ src/board/Board.jsx
@@ -1,107 +1,8 @@
 import React, { useEffect, useReducer, useRef } from "react";
 import { motion } from "framer-motion";
-import "./board.css";
 import getRandomBoard from "../utils/utils.js";
-
-// const BoardContext = React.createContext()
-// BoardContext.displayName = 'BoardContext'
-
-function cellReducer(state, action) {
-  switch (action.type) {
-    case "hide":
-      return {
-        ...state,
-        state: "hide",
-      };
-    case "found":
-      return {
-        ...state,
-        state: "found",
-      };
-    case "cross":
-      return {
-        ...state,
-        state: "cross",
-      };
-    default:
-      throw new Error("Unknown action type in cellReducer");
-  }
-}
-
-const Cell = ({ num, boardState, boardDispatch }) => {
-  const [cellState, cellDispatch] = useReducer(cellReducer, {
-    state: "hide", // hide|found
-  });
-
-  const timerId = useRef(null);
-
-  useEffect(() => {
-    // console.log(`useEffect:Cell num: ${num}`)
-    cellDispatch({ type: "found" });
-    const timerId = setTimeout(() => {
-      cellDispatch({ type: "hide" });
-    }, 1000);
-
-    return () => clearTimeout(timerId);
-  }, [num]);
-
-  useEffect(() => {
-    // console.log(`useEffect:Cell num: ${num}`)
-    if (boardState.state === "reset") {
-      cellDispatch({ type: "found" });
-      const timerId = setTimeout(() => {
-        cellDispatch({ type: "hide" });
-      }, 1000);
-    }
-
-    return () => clearTimeout(timerId);
-  }, [boardState]);
-
-  function showText(state, num) {
-    switch (state) {
-      case "hide":
-        return "?";
-      case "found":
-        return num;
-      case "cross":
-        return "X";
-      default:
-        throw new Error("Unknown action type in showText");
-    }
-  }
-
-  const handleClick = (num, cellState, boardState) => {
-    if (num === boardState.searchNum) {
-      cellDispatch({ type: "found" });
-      boardDispatch({ type: "correctClick" });
-    } else {
-      // Do nothing if cell is already found
-      if (cellState.state === "found") {
-        return;
-      }
-      cellDispatch({ type: "cross" });
-      timerId.current = setTimeout(() => {
-        cellDispatch({ type: "hide" });
-      }, 500);
-    }
-    // console.log(`handleClick: num: ${num}, searchNum: ${boardState.searchNum}`);
-    return () => {
-      if (timerId.current) {
-        clearTimeout(timerId.current);
-      }
-    };
-  };
-
-  return (
-    <motion.div
-      whileHover={{ scale: 1.1}}
-      whileTap={{ scale: 0.9 }}
-      onClick={() => handleClick(num, cellState, boardState)}
-    >
-      {showText(cellState.state, num)}
-    </motion.div>
-  );
-};
+import Cell from './Cell'
+import "./board.css";
 
 function boardReducer(state, action) {
   switch (action.type) {
@@ -202,8 +103,13 @@ const Board = () => {
             boardDispatch={boardDispatch}
           />
         </div>
-        <motion.button whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }} onClick={resetBoard}>Reset</motion.button>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={resetBoard}
+        >
+          Reset
+        </motion.button>
       </div>
     </>
   );
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,7 @@
 // Give a random integer between 0 and num, 0 inclusive
 const giveRandom = (num) => Math.floor(Math.random(num) * num);
 
-// Print an array of length 9 into 3x3 matrix
-// const printBoard = boardArr => {
-//   const chunkSize = 3;
-//   for (let i = 0; i < boardArr.length; i += chunkSize) {
-//   const chunk = boardArr.slice(i, i + chunkSize);
-//   console.log(chunk)
-//   }
-// }
-
+// Return a random board (array with 9 numbers)
 const getRandomBoard = () => {
   const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const boardArray = [];
